f2ac432bbb2242648c7ff17a9f571463c28d0269
Gisselle <gissellebenavidez35@gmail.com>
2018-05-13 14:48:57 +0000

    message

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -25,4 +25,16 @@ class TodoController < ApplicationController
          end
         @color="purple"
     end
+    
+    def new
+    
+    end
+    
+    def create
+     t = Todo.new
+     t.description = params['description']
+     t.pomodoro_estimate = params['pomodoro_estimate']
+     t.save
+     redirect_to "/todo/show/#{ t.id }"
+    end
     end
